ba07ec3e2d0c398d904f538e1a76f40c4d169a66
codechaitu <codechaitu@mercari.com>
2019-02-18 11:58:44 +0900

    changed the description of output that function will return

--- server/server_test.go
+++ server/server_test.go
@@ -36,7 +36,7 @@ var _ = Describe("Server", func() {
 				Expect(response.Code).To(Equal(200))
 			})
 
-			It("Returns the String 'Hello World'", func() {
+			It("Returns the String 'User, you will be dropped soon.'", func() {
 				Expect(response.Body.String()).To(Equal("User, you will be dropped soon."))
 			})
 		})
